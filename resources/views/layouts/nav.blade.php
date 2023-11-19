2754664e6d00910234438a34e0a502466c67abc8
SilentReza <mahvashireza@yahoo.com>
2023-11-19 19:37:56 +0330

    my tasks done !

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,18 +1,5 @@
-<nav class="navbar navbar-expand-lg bg-light py-1" style="background-color: hsl(218, 41%, 15%);
-            background-image: radial-gradient(650px circle at 0% 0%,
-            hsl(218, 41%, 35%) 15%,
-            hsl(218, 41%, 30%) 35%,
-            hsl(218, 41%, 20%) 75%,
-            hsl(218, 41%, 19%) 80%,
-            transparent 100%),
-            radial-gradient(1250px circle at 100% 100%,
-                hsl(218, 41%, 24%) 15%,
-                hsl(218, 41%, 21%) 35%,
-                hsl(218, 41%, 16%) 80%,
-                hsl(218, 41%, 19%) 80%,
-                transparent 100%)">
+<nav class="navbar navbar-expand-lg bg-light py-3" style="background-color: transparent !important;z-index: 999">
     <div class="container">
-{{--        <a class="navbar-brand" href="{{ URL('/dashboard') }}" style="color: #cbd5e0">Dashboard</a>--}}
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -30,9 +17,9 @@
                     </li>
                 @else
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
                            aria-expanded="false">
-                            {{ Auth::user()->name }}
+                            {{ Auth::user()->full_name}}
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="{{ route('logout') }}"
